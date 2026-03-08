27ffe9dd3986d2090e041679cff3700ea682b847
Obed jorge <Obedjorge22@gmail.com>
2026-03-08 21:13:31 +0100

    docs: adding java progect

---
+++ services/backend-api/Qrbank/src/test/java/backend_api/Qrbank/QrbankApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_api.Qrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
