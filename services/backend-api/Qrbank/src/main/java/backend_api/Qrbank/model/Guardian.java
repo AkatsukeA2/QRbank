5e22a12c8822c32cfcc803893b48b9876ee9711d
Obed jorge <Obedjorge22@gmail.com>
2026-03-22 17:25:47 +0100

    add: model

---
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/model/Guardian.java
@@ -0,0 +1,63 @@
+package backend_api.Qrbank.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("guardians")
+public class Guardian {
+
+    @Id
+    @Column("id")
+    private Long id;
+
+    @NotBlank
+    @Column("first_name")
+    private String firstName;
+
+    @NotBlank
+    @Column("last_name")
+    private String lastName;
+
+    @NotBlank
+    @Email
+    @Column("email")
+    private String email;
+
+    @NotBlank
+    @Column("phone_number")
+    private String phoneNumber;
+
+    @NotBlank
+    @Column("relationship")
+    private Relationship relationship;
+
+    @NotBlank
+    @Column("created_at")
+    private LocalDateTime createdAT;
+
+    @NotBlank
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+
+    @NotBlank
+    @Column("deleted_at")
+    private LocalDateTime deletedAt;
+
+
+
+
+
+
+
+}
