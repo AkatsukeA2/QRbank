884f828924707678984f0f32e6837b9996b99e5d
Obed jorge <Obedjorge22@gmail.com>
2026-03-22 19:39:51 +0100

    adding: service

--- services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/repository/GuardianRepository.java
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/repository/GuardianRepository.java
@@ -1,9 +1,16 @@
 package backend_api.Qrbank.repository;
 
+import backend_api.Qrbank.dto.GuardianResponseDTO;
 import backend_api.Qrbank.model.Guardian;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface GuardianRepository extends ReactiveCrudRepository<Guardian, Long> {
+    Mono<Boolean> existsByEmail(String email);
+
+    Guardian findByEmail(String email);
 }
