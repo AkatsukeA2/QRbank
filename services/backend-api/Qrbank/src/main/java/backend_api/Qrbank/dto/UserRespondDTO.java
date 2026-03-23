e60ce82b58caba8190b87fe86d33aead53dd1791
Obed jorge <Obedjorge22@gmail.com>
2026-03-23 02:11:39 +0100

    adding: DTOs

---
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/dto/UserRespondDTO.java
@@ -0,0 +1,44 @@
+package backend_api.Qrbank.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record UserRespondDTO(
+
+        @NotBlank
+        Long id,
+
+        @NotBlank
+        String fistName,
+
+        @NotBlank
+        String lastName,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String passWord,
+
+        @NotBlank
+        String phoneNumber,
+
+        @NotBlank
+        Long roleId,
+
+        @NotBlank
+        Long guardianID,
+
+        @NotBlank
+        LocalDateTime createdAt,
+
+        @NotBlank
+        LocalDateTime updatedAt,
+
+        @NotBlank
+        LocalDateTime deletedAt
+) {
+}
