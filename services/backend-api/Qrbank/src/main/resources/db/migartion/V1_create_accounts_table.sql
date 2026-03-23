783c17333a1c4ae96c14529db2cbdc1707e7a941
Obed jorge <Obedjorge22@gmail.com>
2026-03-23 19:52:17 +0000

    adding: migration

---
+++ services/backend-api/Qrbank/src/main/resources/db/migartion/V1_create_accounts_table.sql
@@ -0,0 +1,19 @@
+
+
+CREATE TABLE `accounts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_number` varchar(30) NOT NULL,
+  `iban` varchar(34) NOT NULL,
+  `balance` decimal(19,2) NOT NULL DEFAULT 0.00,
+  `currency` varchar(10) NOT NULL DEFAULT 'AOA',
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account_number` (`account_number`),
+  UNIQUE KEY `iban` (`iban`),
+  KEY `fk_account_user` (`user_id`),
+  CONSTRAINT `fk_account_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+)
