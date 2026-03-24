4fba5b46bf62d1c7ea6319ebf47158a95169db1c
Obed jorge <Obedjorge22@gmail.com>
2026-03-24 00:04:07 +0000

    adding: service

--- services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/repository/AccountRepository.java
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package backend_api.Qrbank.repository;
 
 import backend_api.Qrbank.model.Account;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface AccountRepository extends ReactiveCrudRepository<Account, Long> {
+    Mono<Account>  findByUserID(Long userID);
 }
---
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/service/AccountService.java
@@ -0,0 +1,102 @@
+package backend_api.Qrbank.service;
+
+import backend_api.Qrbank.dto.AccountRequestDTO;
+import backend_api.Qrbank.dto.AccountResponseDTO;
+import backend_api.Qrbank.mapper.AccountMapper;
+import backend_api.Qrbank.model.Account;
+import backend_api.Qrbank.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class AccountService {
+
+    private final AccountRepository repository;
+
+    // create account
+
+    public Mono<AccountResponseDTO> createAccount(AccountRequestDTO requestDTO){
+        Account account = AccountMapper.toEntity(requestDTO);
+        return repository.findById(account.getId())
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found")))
+                .flatMap(account1 -> {
+                    return repository.save(account1).map(AccountMapper::toResponse);
+                });
+
+    }
+
+    // find by id
+
+    public Mono<AccountResponseDTO> findByID(Long id){
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found")))
+                .map(AccountMapper::toResponse);
+
+    }
+
+
+    // find all
+    public Flux<AccountResponseDTO> findByAll(){
+        return repository.findAll().filter(account -> account.getDeletedAt() != null && !account.isActive())
+                .map(AccountMapper::toResponse);
+    }
+
+    // find by userID
+    public Mono<AccountResponseDTO> findByUser(Long userID){
+         return repository.findByUserID(userID)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found")))
+                 .map(AccountMapper::toResponse);
+    }
+
+    // unactive account
+    public Mono<AccountResponseDTO> deactivate(Long id){
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found, cannot deactivate")))
+                .flatMap(account -> {
+                    account.setActive(false);
+                    return repository.save(account).map(AccountMapper::toResponse);
+                });
+    }
+
+    // soft delete
+    public Mono<Void> softDelete(Long id){
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found, cannot delete")))
+                .flatMap(account -> {
+                    account.setDeletedAt(LocalDateTime.now());
+                    return repository.save(account);
+                }).then();
+    }
+
+    // restore
+    public Mono<Void> restoreAccount(Long id){
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found, cannot restore")))
+                .flatMap(account -> {
+                    account.setDeletedAt(null);
+                    return repository.save(account);
+                }).then();
+    }
+
+    // hard delete
+    public Mono<Void> hardDelete(Long id){
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Account not found")))
+                .flatMap(account -> {
+                    account.setDeletedAt(LocalDateTime.now());
+                    return repository.deleteById(id);
+                }).then();
+    }
+
+
+
+
+
+
+
+}
