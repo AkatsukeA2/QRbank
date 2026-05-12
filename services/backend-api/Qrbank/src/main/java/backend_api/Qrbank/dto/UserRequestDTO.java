13af21c20a016d952ac24ae3c7f91673b66f8620
Obed jorge <Obedjorge22@gmail.com>
2026-05-12 11:24:17 +0100

    adding docker

--- services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/dto/UserRequestDTO.java
+++ services/backend-api/Qrbank/src/main/java/backend_api/Qrbank/dto/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package backend_api.Qrbank.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -31,7 +32,7 @@ public record UserRequestDTO(
         @NotNull
         LocalDate dateOfBirth,
 
-        @NotNull
+        @Nullable
         Long guardianID
 
 ) {
